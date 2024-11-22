f4d8e36f41e7c2b912db0c0c7aa4e1d50fd27d07
Jens Petersen <petersen@redhat.com>
2024-11-22 20:12:32 +0800

    example: rename snap to stream and use snap <-

--- example/Main.lean
+++ example/Main.lean
@@ -2,10 +2,10 @@ import CurlJson
 
 open Lean
 
-def getSnapshot(snap: String): IO (Except String String) := do
+def getSnapshot(stream: String): IO (Except String String) := do
   let json <- curlGetJson "https://stackage-haddock.haskell.org/snapshots.json"
-  pure $ json.getObjVal? snap >>= Json.getStr?
+  pure $ json.getObjVal? stream >>= Json.getStr?
 
 def main : IO Unit := do
-  let snap := (<- getSnapshot "nightly")
+  let snap <- getSnapshot "nightly"
   IO.println (<- IO.ofExcept snap)
