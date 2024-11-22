043fd96c39c53f7901973a4ab4e26223e75f0099
Jens Petersen <petersen@redhat.com>
2024-11-22 18:24:09 +0800

    initial commit with curlGetJson with ASL license

---
+++ CurlJson/Basic.lean
@@ -0,0 +1,25 @@
+import Lean.Data.Json
+import Curl
+
+open Lean
+open Curl
+
+def curlGetJson (curl : Handle) (url : String): IO Json := do
+    let response ← IO.mkRef { : IO.FS.Stream.Buffer}
+    let h ← IO.mkRef { : IO.FS.Stream.Buffer}
+
+    curl_set_option curl (CurlOption.URL url)
+    curl_set_option curl (CurlOption.VERBOSE 0)
+    curl_set_option curl (CurlOption.FOLLOWLOCATION 1)
+    curl_set_option curl (CurlOption.WRITEDATA response)
+    curl_set_option curl (CurlOption.WRITEFUNCTION Curl.writeBytes)
+    curl_set_option curl (CurlOption.HEADERDATA h)
+    curl_set_option curl (CurlOption.HEADERFUNCTION Curl.writeBytes)
+    curl_easy_perform curl
+
+    let headers := String.fromUTF8! (← h.get).data
+    if List.any (Curl.getHeaderData headers) (·.status = 200)
+    then
+      let bytes ← response.get
+      IO.ofExcept $ Json.parse $ String.fromUTF8! bytes.data
+    else throw (IO.userError s!"url not found")
---
+++ README.md
@@ -0,0 +1,4 @@
+# curl-json
+
+Combines the [Curl](https://reservoir.lean-lang.org/@bergmannjg/Curl)
+and Lean.Data.Json to easily get json data from an URL.
