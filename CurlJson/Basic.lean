2e0260316c2f8e59aec03307fb20a6320d9d1cb7
Jens Petersen <petersen@redhat.com>
2024-11-23 18:16:37 +0800

    Basic: use ascii <-

--- CurlJson/Basic.lean
+++ CurlJson/Basic.lean
@@ -4,11 +4,11 @@ import Curl
 open Lean
 open Curl
 
--- Fixme: leak: missing curl_easy_cleanup at end
+-- Fixme: should have curl_easy_cleanup at end
 def curlGetJson (url : String): IO Json := do
-    let curl ← curl_easy_init
-    let response ← IO.mkRef { : IO.FS.Stream.Buffer}
-    let h ← IO.mkRef { : IO.FS.Stream.Buffer}
+    let curl <- curl_easy_init
+    let response <- IO.mkRef { : IO.FS.Stream.Buffer}
+    let h <- IO.mkRef { : IO.FS.Stream.Buffer}
 
     curl_set_option curl (CurlOption.URL url)
     curl_set_option curl (CurlOption.VERBOSE 0)
@@ -19,9 +19,9 @@ def curlGetJson (url : String): IO Json := do
     curl_set_option curl (CurlOption.HEADERFUNCTION Curl.writeBytes)
     curl_easy_perform curl
 
-    let headers := String.fromUTF8! (← h.get).data
+    let headers := String.fromUTF8! (<- h.get).data
     if List.any (Curl.getHeaderData headers) (·.status = 200)
     then
-      let bytes ← response.get
+      let bytes <- response.get
       IO.ofExcept $ Json.parse $ String.fromUTF8! bytes.data
     else throw (IO.userError s!"url not found")
